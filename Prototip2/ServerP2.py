311869988c0c9e09bb6ec133cf90c94f18df1372
hhosp <148861757+hhosp@users.noreply.github.com>
2025-03-04 14:50:09 +0100

    ServerP2 1.0

--- Prototip2/ServerP2.py
+++ Prototip2/ServerP2.py
@@ -9,7 +9,27 @@ for user in dadesServer.users:
 #Prova de la classe User
 provaUser = User(id=1, username="Kurl", password="12345", email="prova2@gmail.com")
 print(provaUser)
+#Prova de la classe Child
+provaChild = Child(id=1, child_name = "Mohamed", sleep_average = 8, treatment_id = 1, time = 0)
+print(provaChild)
 
-class DAOUsers:
+class DAOUser:
     def __init__(self):
         self.users = dadesServer.users
+
+    def getUserById(self,user_id):
+        for user in self.users:
+            if user_id == user.id:
+                return user
+            return None
+
+class DAOChild:
+    def __init__(self):
+        self.children = dadesServer.children
+
+    def getChildByUserId(self, user_id):
+        child_ids = [rel["child_id"] for rel in dades.relation_user_child if rel["user_id"] == user_id]
+        return [child for child in self.children if child.id in child_ids]
+        
+    def getTapByChild(self, child_id):
+        pass 
